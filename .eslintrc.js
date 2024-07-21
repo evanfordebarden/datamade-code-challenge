a985301c2e4b2e68d00ee413d3876439b1805feb
efb <evanbarden@gmail.com>
2024-07-21 18:59:06 -0400

    Step 5: Add unit tests and resolve linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,19 @@
+"use strict"
 module.exports = {
   globals: {
     __PATH_PREFIX__: true,
   },
+  parserOptions: {
+    ecmaVersion: 8,
+  },
   rules: {
-    "indent": ["error", 2],
+    indent: ["error", 2],
     "no-console": "off",
-    "strict": ["error", "global"],
-    "curly": "warn",
-    "semi": ["error", "never"],
+    strict: ["error", "global"],
+    curly: "warn",
+    semi: ["error", "never"],
     "space-in-parens": ["error", "never"],
     "space-before-function-paren": ["error", "always"],
-    "space-before-blocks": ["error", "always"]
-  }
+    "space-before-blocks": ["error", "always"],
+  },
 }
--- tests/test_views.py
+++ tests/test_views.py
@@ -2,14 +2,37 @@ import pytest
 
 
 def test_api_parse_succeeds(client):
-    # TODO: Finish this test. Send a request to the API and confirm that the
-    # data comes back in the appropriate format.
+
     address_string = '123 main st chicago il'
-    pytest.fail()
+
+    # make an api call with the given address_string
+    response = client.get('/api/parse/', {'input_string': address_string})
+
+    # test response for appropriate status code, data keys, and types of data
+    # returned in those keys
+    try:
+        assert response.status_code == 200
+        assert 'input_string' in response.data
+        assert 'address_components' in response.data
+        assert 'address_type' in response.data
+        assert isinstance(response.data['input_string'], str)
+        assert isinstance(response.data['address_components'], dict)
+        assert isinstance(response.data['address_type'], str)
+    except AssertionError as e:
+        pytest.fail(f"Test failed: {e}")
 
 
 def test_api_parse_raises_error(client):
-    # TODO: Finish this test. The address_string below will raise a
-    # RepeatedLabelError, so ParseAddress.parse() will not be able to parse it.
+
     address_string = '123 main st chicago il 123 main st'
-    pytest.fail()
+    # make an api call with the given address_string
+    response = client.get('/api/parse/', {'input_string': address_string})
+
+    # test response for appropriate (failed) status code, data key, and
+    # error message within detail.
+    try:
+        assert response.status_code == 400  # Bad Request
+        assert 'detail' in response.data
+        assert 'Error parsing address' in response.data['detail']
+    except AssertionError as e:
+        pytest.fail(f"Test failed: {e}")
